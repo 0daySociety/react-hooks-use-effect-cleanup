7367e2165d2169471d266a87fc5d4601ef3ce6e7
--replace <david.thuku@studentmoringa.school.com>
2023-07-25 12:15:37 +0300

    Done with  op lab

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,16 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    console.log("calling my spirit")
+    let clean=setInterval(() => {
       setTime(new Date());
     }, 1000);
+    return function cleanup(){
+      clearInterval(clean)
+    }
+    
+
+   
   }, []);
 
   return <div>{time.toString()}</div>;
